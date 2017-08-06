152cbe92892795fc6e9e1ce0eab7b291e0b3dd75
CaoGuoRong <cet48818@hotmail.com>
2017-08-06 13:53:59 +0800

    new

--- react-sell/README.md
+++ react-sell/README.md
@@ -1,14 +1,4 @@
-# React仿制饿了么webapp的商家页面
-
-### 安装
-
-    git clone https://github.com/cet48818/react-sell.git
-    cd react-sell
-    yarn install
-
-### 运行
-
-    yarn start 
+### React仿制饿了么webapp的商家页面
 
 ### 说明
 
--- react-sell/config/webpack.config.prod.js
+++ react-sell/config/webpack.config.prod.js
@@ -67,7 +67,7 @@ module.exports = {
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     // publicPath: '.' + publicPath
-    publicPath: publicPath
+    publicPath: '.' + publicPath
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
